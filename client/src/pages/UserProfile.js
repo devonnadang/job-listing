1ff7f59d6344efd873d9f8f31dbe09cc9261a6fd
Devonna Dang <dang.devonna@gmail.com>
2022-03-11 15:48:23 -0800

    created input skills and a button to add skills (no functionality yet). added interests section

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -5,7 +5,10 @@ import ExpDetail from "../component/ExpDetail"
 import SkillTag from "../component/SkillTag";
 import Stack from '@mui/material/Stack';
 import Navigation from './Navigation';
-
+import styles from './UserProfile.module.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 
 
 
@@ -72,39 +75,59 @@ function UserProfile(props) {
         alert("delete skill function here");
     }
 
+    function addSkill(){
+        alert("add skill function here");
+    }
+
+    function editInterests() {
+        alert("edit interests function here");
+    }
+
     return  (
         <div>
             <Navigation />
-            <h1> Jane Doe </h1>
-            <h2> Education Details </h2>
-            {
-                schools.map((school) => (<EduDetail 
-                    name={school.name} 
-                    start={school.start}
-                    end={school.end}
-                    major={school.major}
-                    gpa={school.gpa}/>))
-            }
-            <button onClick={editSchools}> Add School </button>
-            <h2> Experience Details </h2>
-            <ExpDetail 
-                title="Software Developer Intern" 
-                company="Apple" 
-                start="May 2020" 
-                end="Aug 2020" 
-                locationCity="Cupertino" 
-                locationState="CA"
-                description="
-                -Developed 3 projects during duration. -Collaborated in a team of 10" 
-                />
-            <h2> Skills </h2>
-            <Stack spacing={2} direction="row" alignItems="center" justifyContent="center">
-            {
-                skills.map((skill) => <SkillTag name={skill.skill} deleteSkill={deleteSkill}/>)
-            }
-            </Stack>
-        
-            <h2> Your Interests </h2>
+
+            <div className={styles.main}>
+                <h1> Jane Doe </h1>
+                <Avatar sx={{width:100, height: 100}}> </Avatar>
+                <h2> Education Details </h2>
+                {
+                    schools.map((school) => (<EduDetail 
+                        name={school.name} 
+                        start={school.start}
+                        end={school.end}
+                        major={school.major}
+                        gpa={school.gpa}/>))
+                }
+                <Button onClick={editSchools} variant="outlined"> Add School </Button>
+                
+                <h2> Experience Details </h2>
+                <ExpDetail 
+                    title="Software Developer Intern" 
+                    company="Apple" 
+                    start="May 2020" 
+                    end="Aug 2020" 
+                    locationCity="Cupertino" 
+                    locationState="CA"
+                    description="
+                    -Developed 3 projects during duration. -Collaborated in a team of 10" 
+                    />
+               
+                <h2> Skills </h2>
+                <Stack spacing={2} direction="row" alignItems="center" justifyContent="center">
+                {
+                    skills.map((skill) => <SkillTag name={skill.skill} deleteSkill={deleteSkill}/>)
+                }
+                </Stack>
+                <TextField variant="outlined" size="small"> </TextField> <Button variant ="outlined" onClick={addSkill}> Add </Button>
+                
+                <h2> Your Interests <Button variant="text" onClick={editInterests}> Edit </Button> </h2>
+                <p> Job Schedule </p>
+                <p> Cities </p>
+                <p> Roles </p>
+
+            </div>
+            
         </div>
 
     );
