87e36135b5709a934a83c3e1f420fd615db2a4a7
mike <michaelcheung882@gmail.com>
2022-04-04 20:25:26 -0700

    idk

---
+++ client/src/config/config.js
@@ -0,0 +1,7 @@
+const dbconfig = {
+    host: "job-finder-db.cyljkfko1xny.us-west-1.rds.amazonaws.com",
+    user: "admin",
+    password: "masterROOTadmin160",
+    database: "job_finder"
+}
+module.exports = dbconfig;
