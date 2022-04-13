24f8e290b1a1e92f3474f63b90b538a7ca844b6c
Barnabas Yuen <barnabas.yuen@sjsu.edu>
2022-04-13 12:27:11 -0700

    changed file structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const dbconfig = require('../config/config.js')
+const dbconfig = require('./config/config.js')
 var mysql = require('mysql');
 
 app.use(express.json())
